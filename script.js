dd930fc2581ca27cd9d07b35d5873f3c16b34ae5
Neozus <114140035+Neozus@users.noreply.github.com>
2022-10-12 15:59:00 +0200

    Calculator tableau

---
+++ script.js
@@ -0,0 +1,15 @@
+let btns = Array.from(document.querySelectorAll('button'));
+console.log(btns);
+const display = document.querySelector(".visio");
+btns.forEach('button') => { 'click', ()  }
+console.log(btns);
+
+function hello () {
+    display.textContent = 'Hello';
+    console.log('Bonjour');
+}
+
+btns.addEventListener('click', hello)
+console.log(btns);
+
+
